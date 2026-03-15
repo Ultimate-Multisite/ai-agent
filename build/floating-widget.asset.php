1d06afa9a27c0385e69a522c39ed987b1ccd75e1
David Stone <david@nnucomputerwhiz.com>
2026-03-15 20:10:19 +0000

    chore: rebuild assets with @wordpress/scripts 31.6.0

--- build/floating-widget.asset.php
+++ build/floating-widget.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a4c2ebc67a8e3c0c5052');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ba81bee8109d90d7334a');
